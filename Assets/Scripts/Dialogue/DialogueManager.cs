503ad0e3a1495b84df703341ce4a3fb38c88757f
KatoriHasBeenTaken <tuganing@gmail.com>
2024-11-18 02:50:46 +0700

    1

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DialogueManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public TextMeshProUGUI dialogueText;
+    public GameObject dialogueBox;
+    private Queue<string> sentences;
+    [System.Serializable]
+    public class Dialogue
     {
-        
+        public string npcName;
+        [TextArea(3, 10)]
+        public string[] sentences; // Array for multiple lines of dialogue.
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        sentences = new Queue<string>();
+        dialogueBox.SetActive(false); // Hide dialogue box initially
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        dialogueBox.SetActive(true);
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence; // Update UI text
+    }
+
+    void EndDialogue()
+    {
+        dialogueBox.SetActive(false);
+        Debug.Log("End of Dialogue.");
     }
 }
